3e7bb904880fc0211fb62816fb0b6adaccd1446b
Petar Maymounkov <petarm@gmail.com>
2022-11-13 13:23:36 -0800

    import gov4git/lib into its own go module here

---
+++ git/git_test.go
@@ -0,0 +1,18 @@
+package git
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestRenameMain(t *testing.T) {
+	ctx := context.Background()
+	dir := t.TempDir()
+	fmt.Println(dir)
+	repo := InitPlain(ctx, dir, false)
+	ChangeDefaultBranch(ctx, repo, MainBranch)
+	<-(chan int)(nil)
+}
+
+// TODO: test fetching from a private git
