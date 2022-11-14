c189f699c5b9bf869ed0db2cc56b3fb8a7e2d94f
Petar Maymounkov <petarm@gmail.com>
2022-11-14 11:54:31 -0800

    fix tree rename

--- git/tree.go
+++ git/tree.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"path/filepath"
 
+	"github.com/go-git/go-git/v5/plumbing/format/index"
 	"github.com/gov4git/lib4git/form"
 	"github.com/gov4git/lib4git/must"
 )
@@ -37,9 +38,29 @@ func TryFromFile[V form.Form](ctx context.Context, t *Tree, filePath string) (v 
 }
 
 func RenameStage(ctx context.Context, t *Tree, oldPath, newPath string) {
+	stat, err := t.Filesystem.Stat(oldPath)
+	must.NoError(ctx, err)
+	if stat.IsDir() {
+		renameStageDir(ctx, t, oldPath, newPath)
+	} else {
+		renameStageFile(ctx, t, oldPath, newPath)
+	}
+}
+
+func renameStageDir(ctx context.Context, t *Tree, oldPath, newPath string) {
+	infos, err := t.Filesystem.ReadDir(oldPath)
+	must.NoError(ctx, err)
+	for _, info := range infos {
+		RenameStage(ctx, t, filepath.Join(oldPath, info.Name()), filepath.Join(newPath, info.Name()))
+	}
+}
+
+func renameStageFile(ctx context.Context, t *Tree, oldPath, newPath string) {
 	must.NoError(ctx, t.Filesystem.Rename(oldPath, newPath))
 	_, err := t.Remove(oldPath)
-	must.NoError(ctx, err)
+	if err != index.ErrEntryNotFound {
+		must.NoError(ctx, err)
+	}
 	_, err = t.Add(newPath)
 	must.NoError(ctx, err)
 }
