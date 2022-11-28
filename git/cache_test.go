5a2cad2d05e53fb01a789c8629707a8064f66f56
Petar Maymounkov <petarm@gmail.com>
2022-11-28 09:22:24 -0800

    skeleton cache test

---
+++ git/cache_test.go
@@ -0,0 +1,16 @@
+package git
+
+import (
+	"testing"
+)
+
+func TestCache(t *testing.T) {
+	// base.LogVerbosely()
+	// ctx := context.Background()
+	// dir := t.TempDir()
+
+	// rem := InitPlain(ctx, dir, false)
+	// populate(ctx, rem, "ok", true)
+
+	// cache :=
+}
