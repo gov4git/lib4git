872526fa3f7c7aedf409470d7f7e624a83c6dcd6
Petar Maymounkov <petarm@gmail.com>
2023-01-31 19:22:06 -0800

    add file utils

---
+++ file/file.go
@@ -0,0 +1,35 @@
+package file
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+
+	"github.com/go-git/go-billy/v5"
+	"github.com/gov4git/lib4git/must"
+	"github.com/gov4git/lib4git/ns"
+)
+
+func StringToFile(ctx context.Context, fs billy.Filesystem, path ns.NS, content string) {
+	BytesToFile(ctx, fs, path, []byte(content))
+}
+
+func FileToString(ctx context.Context, fs billy.Filesystem, path ns.NS) string {
+	return string(FileToBytes(ctx, fs, path))
+}
+
+func BytesToFile(ctx context.Context, fs billy.Filesystem, path ns.NS, content []byte) {
+	file, err := fs.OpenFile(path.Path(), os.O_CREATE|os.O_RDWR, 0644)
+	must.NoError(ctx, err)
+	defer file.Close()
+	_, err = file.Write(content)
+	must.NoError(ctx, err)
+}
+
+func FileToBytes(ctx context.Context, fs billy.Filesystem, path ns.NS) []byte {
+	file, err := fs.Open(path.Path())
+	must.NoError(ctx, err)
+	content, err := ioutil.ReadAll(file)
+	must.NoError(ctx, err)
+	return content
+}
