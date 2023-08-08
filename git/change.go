3225ff8d628079f7cb9b5c940e20d767d7d78720
Petar Maymounkov <petarm@gmail.com>
2023-08-08 10:25:51 -0700

    expand Change description structure

--- git/change.go
+++ git/change.go
@@ -2,9 +2,32 @@ package git
 
 import "github.com/gov4git/lib4git/form"
 
-type Change[R any] struct {
-	Result R
-	Msg    string //commit msg
+type Change[Q any, R any] struct {
+	Msg    string     `json:"msg"`
+	Fn     string     `json:"fn"`
+	Query  Q          `json:"query"`
+	Result R          `json:"result"`
+	Steps  form.Forms `json:"steps"`
 }
 
-type ChangeNoResult = Change[form.None]
+func NewChange[Q any, R any](
+	msg string,
+	fn string,
+	query Q,
+	result R,
+	steps form.Forms,
+) Change[Q, R] {
+	return Change[Q, R]{
+		Msg:    msg,
+		Fn:     fn,
+		Query:  query,
+		Result: result,
+		Steps:  steps,
+	}
+}
+
+type ChangeNoResult = Change[form.None, form.None]
+
+func NewChangeNoResult(msg string, fn string) ChangeNoResult {
+	return NewChange(msg, fn, form.None{}, form.None{}, nil)
+}
