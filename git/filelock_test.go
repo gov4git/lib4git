e12cba389c9c4659cb6e8a46319c27aa0f42e4d8
Petar Maymounkov <petarm@gmail.com>
2023-08-22 13:14:58 -0700

    draft of file-locking repo cache replicas

--- git/filelock_test.go
+++ git/filelock_test.go
@@ -1,9 +1,11 @@
 package git
 
 import (
+	"context"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/gofrs/flock"
 )
@@ -14,18 +16,36 @@ func TestFileLock(t *testing.T) {
 	flk1 := flock.New(lockFile)
 	locked1, err1 := flk1.TryLock()
 	if err1 != nil {
-		t.Errorf("lock 1 (%v)", err1)
+		t.Errorf("lock#1 (%v)", err1)
 	}
 	if !locked1 {
-		t.Errorf("expecting lock to succeed")
+		t.Errorf("expecting lock#1 to succeed")
 	}
 
 	flk2 := flock.New(lockFile)
 	locked2, err2 := flk2.TryLock()
 	if err2 != nil {
-		t.Errorf("lock 2 (%v)", err2)
+		t.Errorf("lock#2 (%v)", err2)
 	}
 	if locked2 {
-		t.Errorf("expecting lock to fail")
+		t.Errorf("expecting lock#2 to fail")
 	}
+
+	// unlock lock#1 in one second
+	go func() {
+		time.Sleep(time.Second)
+		if err := flk1.Unlock(); err != nil {
+			t.Errorf("unlock 1 (%v)", err)
+		}
+	}()
+
+	flk3 := flock.New(lockFile)
+	locked3, err3 := flk3.TryLockContext(context.Background(), time.Second*2)
+	if err3 != nil {
+		t.Errorf("lock#3 (%v)", err3)
+	}
+	if !locked3 {
+		t.Errorf("expecting lock#3 to succeed")
+	}
+
 }
