0fa8cb8f54c3409d62d1d22a682171b6da75e8d7
Petar Maymounkov <petarm@gmail.com>
2023-09-18 10:05:33 -0700

    add option to use cache in test context (#19)

--- testutil/ctx.go
+++ testutil/ctx.go
@@ -2,16 +2,24 @@ package testutil
 
 import (
 	"context"
+	"math/rand"
+	"path/filepath"
 	"strconv"
 	"sync"
+	"testing"
 
 	"github.com/gov4git/lib4git/git"
 )
 
-func NewCtx() context.Context {
+func NewCtx(t *testing.T, useCache bool) context.Context {
 	ctx := git.WithAuth(context.Background(), nil)
 	ctx = git.WithTTL(ctx, nil)
-	return context.WithValue(ctx, counterKey{}, &counter{})
+	ctx = context.WithValue(ctx, counterKey{}, &counter{})
+	if useCache {
+		return git.WithCache(ctx, filepath.Join(t.TempDir(), "cache-"+strconv.FormatUint(uint64(rand.Int63()), 36)))
+	} else {
+		return ctx
+	}
 }
 
 type counterKey struct{}
